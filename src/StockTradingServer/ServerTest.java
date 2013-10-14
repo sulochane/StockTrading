5c46c3957507fd99581c957336d771e0c651c21d
dkarmazi <dkarmazi@gwu.edu>
2013-10-14 17:18:08 -0400

    added DB connection

--- src/StockTradingServer/ServerTest.java
+++ src/StockTradingServer/ServerTest.java
@@ -4,6 +4,13 @@
  */
 package StockTradingServer;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 
 public class ServerTest {
     public static void main(String[] args) 
@@ -14,5 +21,69 @@ public class ServerTest {
         client.Test();
         StockTradingCommon.CommonTest common = new StockTradingCommon.CommonTest();
         common.Test();
+        
+        
+        
+        
+        
+        
+		DatabaseConnector dc = new DatabaseConnector();
+		dc.connectToDatabase();
+		Connection con = dc.getCon();
+
+		// TODO Auto-generated method stub
+		Statement st = null;
+		ResultSet rs = null;
+
+		String url = "jdbc:mysql://192.30.164.204:3306/repo6545";
+		String user = "repo6545";
+		String password = "MF4@2163G!8d2L4";
+
+		try {
+			st = con.createStatement();
+			
+
+			// rs = st.executeQuery("SELECT VERSION()");
+			rs = st.executeQuery("SHOW TABLES");
+
+			if (rs.next()) {
+				System.out.println(rs.getString(1));
+			}
+
+			// st.execute(sql)
+
+		} catch (SQLException ex) {
+			Logger lgr = Logger.getLogger(DatabaseConnector.class.getName());
+			lgr.log(Level.SEVERE, ex.getMessage(), ex);
+
+		} finally {
+			try {
+				if (rs != null) {
+					rs.close();
+				}
+				if (st != null) {
+					st.close();
+				}
+				if (con != null) {
+					con.close();
+				}
+
+			} catch (SQLException ex) {
+				Logger lgr = Logger
+						.getLogger(DatabaseConnector.class.getName());
+				lgr.log(Level.WARNING, ex.getMessage(), ex);
+			}
+		}
+
+	}
+
+        
+        
+        
+        
+        
+        
+        
+        
     }
 }
