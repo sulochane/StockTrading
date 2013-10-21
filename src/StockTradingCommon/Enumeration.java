35959868917b36a0729c03e3825ec4c716bbee19
sulochane <hirosh@gwmail.gwu.edu>
2013-10-21 11:33:47 -0400

    introduced a new file to keep constant

---
+++ src/StockTradingCommon/Enumeration.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package StockTradingCommon;
+
+/**
+ * @date    :   Oct 21, 2013
+ * @author  :   Hirosh Wickramasuriya
+ */
+public  class Enumeration {
+
+    public static class Database
+    {
+        public final static String    DB_INSERT_SUCCESS = "Insert to the database success.";
+        public final static String    DB_INSERT_FAILURE = "Failed to insert a record.";
+    }
+}
