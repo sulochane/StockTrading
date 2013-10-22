3223f948685fcc0759f7d87dc6594becab7ae054
sulochane <hirosh@gwmail.gwu.edu>
2013-10-21 22:17:26 -0400

    DB_REQUEST_INITIATED introduced

--- src/StockTradingCommon/Enumeration.java
+++ src/StockTradingCommon/Enumeration.java
@@ -13,6 +13,8 @@ public  class Enumeration {
 
     public static class Database
     {
+        public final static String    DB_REQUEST_INITIATED = "Please Wait, your requst is in progress...";
+        
         public final static String    DB_INSERT_SUCCESS = "Record inserted successfully.";
         public final static String    DB_INSERT_FAILURE = "Failed to insert a record.";
         
